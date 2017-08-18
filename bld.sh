2026968acfbcf063afc9db05293ad4e0e702f732
Dave Nicholas <davenicholas747@gmail.com>
2017-08-18 16:33:45 +0100

    Housekeeping.

--- bld.sh
+++ bld.sh
@@ -1,12 +1,16 @@
 echo "CC: Clearing out old build relics..."
 node-gyp clean
+node-pre-gyp clean
+
+rm -rf build
 
 node-gyp --target=v8.1.4 configure
 node-pre-gyp build
 node-pre-gyp package
 
 #node-pre-gyp-github publish
-#node-pre-gyp-github publish --release
+export NODE_PRE_GYP_GITHUB_TOKEN=192387be34b4ce3d39ffc42769b172ccd27b923a
+node-pre-gyp-github publish --release
 
 echo "cc: Run smoke test on addon...."
 
--- cln.sh
+++ cln.sh
@@ -1,4 +1,5 @@
 node-gyp clean
+node-pre-gyp clean
 rm -rf bitcoin
 
 
