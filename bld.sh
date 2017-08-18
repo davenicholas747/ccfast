6d7636fd03ee42f0298181fd4695a97217e7599d
Dave Nicholas <davenicholas747@gmail.com>
2017-08-18 20:27:54 +0100

    Tidy up.

--- bld.sh
+++ bld.sh
@@ -3,13 +3,23 @@ node-gyp clean
 node-pre-gyp clean
 
 rm -rf build
+rm -rf node_modules
+
+npm -l install node-pre-gyp-github
 
 node-gyp --target=v8.1.4 configure
 node-pre-gyp build
 node-pre-gyp package
 
+echo ""
+echo ""
+echo "Testing package..."
+
+#node-pre-gyp testpackage
+
 #node-pre-gyp-github publish
-export NODE_PRE_GYP_GITHUB_TOKEN=192387be34b4ce3d39ffc42769b172ccd27b923a
+#export NODE_PRE_GYP_GITHUB_TOKEN=192387be34b4ce3d39ffc42769b172ccd27b923a
+
 node-pre-gyp-github publish --release
 
 echo "cc: Run smoke test on addon...."
--- cln.sh
+++ cln.sh
@@ -1,5 +1,9 @@
 node-gyp clean
 node-pre-gyp clean
+rm -rf build
+rm -rf node_modules
 rm -rf bitcoin
+
+
 
 
